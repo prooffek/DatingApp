1c2ffd50c4719e755dd4b6f437e71a3953e4be5b
prooffek <prooffek@gmail.com>
2022-04-09 13:53:25 +0200

    Implement login in functionality restricting navbar objects accessibility

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from "./_services/account.service";
+import { User } from "./_models/user";
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,19 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) { }
 
   ngOnInit(): void {
     this.getUser();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem("user"));
+    this.accountService.setCurrentUser(user);
   }
 
   getUser() {
