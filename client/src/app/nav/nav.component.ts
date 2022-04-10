84226b5cf356930218f2f625cca5aa271b7d1cca
prooffek <prooffek@gmail.com>
2022-04-10 17:00:58 +0200

    Implement module management by routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable } from 'rxjs';
-import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -12,7 +12,11 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -20,14 +24,18 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
         next: response => {
-            console.log("response: " + response);
+            this.router.navigateByUrl("/members");
         },
-        error: error => { console.log(error); }
+        error: error => { 
+          console.log(error);
+          this.toastr.error(error.error);
+        }
     });
   }
 
   logout() {
       this.accountService.logout();
+      this.router.navigateByUrl("/");
   }
 
   getCurrentUser() {
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from "../_services/account.service";
 
 @Component({
@@ -10,7 +11,10 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +25,7 @@ export class RegisterComponent implements OnInit {
       console.log(response);
     }, error => {
         console.log(error);
+        this.toastr.error(error.error);
     });
   }
 
