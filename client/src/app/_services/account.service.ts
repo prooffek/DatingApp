84c773b7d7a9806a80a283cce04ca2de8a25520c
prooffek <prooffek@gmail.com>
2022-04-10 10:18:00 +0200

    Implement registration to db

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -34,4 +34,15 @@ export class AccountService {
     localStorage.removeItem("user");
     this.currentUserSource.next(null);
   }
+
+  register(model: any) {
+    return this.httpClient.post(`${this.baseUrl}account/register`, model).pipe(
+      map((user: User) => {
+          if (user) {
+              localStorage.setItem("user", JSON.stringify(user));
+              this.currentUserSource.next(user);
+          }
+      })
+    );
+  }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AccountService } from "../_services/account.service";
 
 @Component({
   selector: 'app-register',
@@ -6,19 +7,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() usersFromHomeComponent;
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe(response => {
+      this.cancel();
+      console.log(response);
+    }, error => {
+        console.log(error);
+    });
   }
 
   cancel() {
-    console.log("cancelled");
+    this.cancelRegister.emit(false);
   }
 }
