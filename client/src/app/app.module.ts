44b8757b1b0b9fffb6e77aae6bdc0ab78fb1228f
prooffek <prooffek@gmail.com>
2022-04-11 17:41:51 +0200

    Section 6 - test-errors component added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { TestErrorsComponent } from './src/app/errors/test-errors/test-errors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    TestErrorsComponent
   ],
   imports: [
     HttpClientModule,
